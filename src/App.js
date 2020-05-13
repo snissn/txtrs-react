17b48f7409397028dce6fac0507b8db9ed0ee836
Mike Seiler <snissn@gmail.com>
2020-05-12 20:13:55 -0400

    use right url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Txt.rs</h1>
           <h3 className="App-subtitle">Connect <a href="http://metamask.io" target="_blank">Metamask.io</a> to the MaticV3 testnet</h3>
-          <h4 className="App-subtitle"> https://testnetv3-explorer.matic.network/ </h4> 
+          <h4 className="App-subtitle"> https://testnetv3.matic.network/ </h4> 
         </header>
        <Route exact path='/' component={Txtrs} />
       </div>
