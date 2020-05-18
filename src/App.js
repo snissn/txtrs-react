83da209c4a9b09f2de0f3e8e9a2d5cdf3e5191ea
Mike Seiler <snissn@gmail.com>
2020-05-18 13:49:24 -0400

    better onboarding text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Txt.rs.2.2.1</h1>
           <h3 className="App-subtitle">Connect <a href="http://metamask.io" target="_blank">Metamask.io</a> to the MaticV3 testnet</h3>
-          <h4 className="App-subtitle"> https://testnetv3.matic.network/ </h4> 
+          <h4 className="App-subtitle"> Add custom testnet RPC url: </h4> 
+          <h5 className="App-subtitle"> https://testnetv3.matic.network/ </h5> 
         </header>
        <Route exact path='/' component={Txtrs} />
       </div>
