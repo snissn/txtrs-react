97837e22d4dc89c9a199f1a6299989c1f96b80dd
mikers <snissn@gmail.com>
2020-05-22 13:09:05 -0400

    new contract

--- src/Web3Helper.js
+++ src/Web3Helper.js
@@ -9,7 +9,7 @@ const EC = require("elliptic").ec;
 const ec = new EC("secp256k1");
 export var w3 = new Web3(window.ethereum);
 var contract_address = "0x6954fd4298F36FE38f254CF6789ebF755bb0035E";
-var contract_address = "0xe312D5107D053355C66C6B9b623D09CE51FB98DD"
+var contract_address = "0x237a497bD0715E1350abAF30db79D4d78b87F7cA"
 export var users_address;
 
 window.w3 = w3;
