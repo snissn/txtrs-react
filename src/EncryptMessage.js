5b3110847c513530d3ba16d8d914d9f076e2e610
mikers <snissn@gmail.com>
2020-11-23 03:21:48 -0500

    change state to props

--- src/EncryptMessage.js
+++ src/EncryptMessage.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Panel from 'react-bootstrap/lib/Panel'
-import {getContract, contract, w3, users_address, getPrivateMessage,colorHash, contrast} from "./Web3Helper"
+import { getContract, contract, w3, users_address, getPrivateMessage, colorHash, contrast } from "./Web3Helper"
 
 import ReactDOM from 'react-dom';
 const ecies = require('eth-ecies')
 
 export default class EncryptMessage extends React.Component {
-	constructor(props) {
+  constructor(props) {
     super(props);
-    console.log("PROPS",props);
+
+    console.log("PROPS", props);
     this.state = {
-      message: props.message,
-      errormessage: props.message
+      secret_message: ""
+
     };
     this.onSendSecretMessage = this.onSendSecretMessage.bind(this);
 
@@ -20,59 +21,49 @@ export default class EncryptMessage extends React.Component {
   }
   onSendSecretMessage = async (event) => {
     event.preventDefault();
-    var private_message = getPrivateMessage(this.state.message.address)
+    var private_message = getPrivateMessage(this.props.message.address)
     // the damn library adds the "0x04" prevailing byte so we need to slice(2) the hex rep
     //https://github.com/libertylocked/eth-ecies/blob/master/index.js#L74
-    var bob_public = this.state.message.bob_public;
-    var public_key = Buffer.from( bob_public.slice(2),'hex');
-    var account = await  w3.eth.getAccounts()
+    var bob_public = this.props.message.bob_public;
+    var public_key = Buffer.from(bob_public.slice(2), 'hex');
+    var account = await w3.eth.getAccounts()
     var encrypt = ecies.encrypt(public_key, this.state.secret_message);
     var contract = getContract();
-    var send = await contract.methods.pm_alice_send_encrypted_message(this.state.message.address,encrypt.toString('hex')).send({gasPrice:0,from:account[0]});
+    var send = await contract.methods.pm_alice_send_encrypted_message(this.props.message.address, encrypt.toString('hex')).send({ gasPrice: 0, from: account[0] });
     return false;
   }
 
   myChangeHandler = (event) => {
-    this.setState({secret_message: event.target.value});
+    this.setState({ secret_message: event.target.value });
 
     return;
-    let nam = event.target.name;
-    let val = event.target.value;
-    let err = '';
-    if (nam === "message") {
-      if (val =="" ) {
-        err = <strong>Your message can't be blank</strong>;
-      }
-    }
-    this.setState({errormessage: err});
-    this.setState({[nam]: val});
   }
   render() {
-            var message = this.state.message;
-            console.log("MESSAGE",message);
-            return(
-<Panel bsStyle="info" key={message.id} className="centeralign">
-            <Panel.Heading>
-              <Panel.Title componentClass="h3">
-										{(() => {
-											switch (message.stage) {
-												case "1":   return "Key request sent.";
-												case "2": return "Encrypted Channel";
-												case "3":  return "Encrypted Message sent to "+message.bob;
-												case "4":  return "Encrypted Message received by "+message.bob;
-												default:      return "unknown stage ";
-											}
-										})()}
-						</Panel.Title>
-            </Panel.Heading>
-            <Panel.Body style={{backgroundColor:colorHash.hex(message.bob), color:contrast(colorHash.hex(message.bob))}}>
-              {this.renderForm(message)}
-            </Panel.Body>
-          </Panel>
-          )
+    var message = this.props.message;
+    console.log("MESSAGE", message);
+    return (
+      <Panel bsStyle="info" key={message.id} className="centeralign">
+        <Panel.Heading>
+          <Panel.Title componentClass="h3">
+            {(() => {
+              switch (message.stage) {
+                case "1": return "Key request sent.";
+                case "2": return "Encrypted Channel";
+                case "3": return "Encrypted Message sent to " + message.bob;
+                case "4": return "Encrypted Message received by " + message.bob;
+                default: return "unknown stage ";
+              }
+            })()}
+          </Panel.Title>
+        </Panel.Heading>
+        <Panel.Body style={{ backgroundColor: colorHash.hex(message.bob), color: contrast(colorHash.hex(message.bob)) }}>
+          {this.renderForm(message)}
+        </Panel.Body>
+      </Panel>
+    )
   }
-  renderForm(message){
-    if(message.stage=='2'){
+  renderForm(message) {
+    if (message.stage == '2') {
       return (
         <form onSubmit={this.onSendSecretMessage} className="form">
           <label htmlFor="Receiver">Receiver</label>
@@ -92,7 +83,7 @@ export default class EncryptMessage extends React.Component {
         </form>
       )
     }
-    if(message.stage=='1'){
+    if (message.stage == '1') {
       return (<span>Conversation with {message.bob} requsted </span>)
     }
     return (<span>Message Sent</span>)
