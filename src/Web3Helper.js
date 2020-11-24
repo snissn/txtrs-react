3e71426a9b49c119bc661a306f13505fa4c0c321
Mike Seiler <snissn@gmail.com>
2020-11-24 00:51:42 -0500

    new wss link

--- src/Web3Helper.js
+++ src/Web3Helper.js
@@ -1,15 +1,14 @@
-import Web3 from 'web3'
+import Web3 from "web3";
 
-
-import abi from './abi_txtrs.json'
-import abi_private_message from './abi_private_message.json'
+import abi from "./abi_txtrs.json";
+import abi_private_message from "./abi_private_message.json";
 
 const EC = require("elliptic").ec;
 const ec = new EC("secp256k1");
 export var w3 = new Web3(window.ethereum);
-export var w3ws = new Web3("wss://txt.mike.rs:443");
+export var w3ws = new Web3("wss://chain.txt.rs:443");
 var contract_address = "0x6954fd4298F36FE38f254CF6789ebF755bb0035E";
-var contract_address = "0x01976Ff8C462E6837413eA72dAE7eC6cC03b497E"
+var contract_address = "0x01976Ff8C462E6837413eA72dAE7eC6cC03b497E";
 export var users_address;
 
 window.w3 = w3;
@@ -29,95 +28,98 @@ export function getBlockNumber(addr) {
 }
 
 export async function getSentMessages() {
-  var account = await w3.eth.getAccounts()
-  var messages_count = await contract.methods.get_sent_messages_total(users_address).call();
-  var messages = []
+  var account = await w3.eth.getAccounts();
+  var messages_count = await contract.methods
+    .get_sent_messages_total(users_address)
+    .call();
+  var messages = [];
   for (var index = messages_count - 1; index >= 0; index--) {
-    var private_message_addr = await contract.methods.get_sent_message(users_address, index).call()
-    var private_message = getPrivateMessage(private_message_addr)
-    var alice = await private_message.methods.alice().call()
-    var bob = await private_message.methods.bob().call()
-    var stage = await private_message.methods.stage().call()
-    var message = { stage: stage, alice: alice, bob: bob, id: index }
+    var private_message_addr = await contract.methods
+      .get_sent_message(users_address, index)
+      .call();
+    var private_message = getPrivateMessage(private_message_addr);
+    var alice = await private_message.methods.alice().call();
+    var bob = await private_message.methods.bob().call();
+    var stage = await private_message.methods.stage().call();
+    var message = { stage: stage, alice: alice, bob: bob, id: index };
 
     if (stage == 1) {
     }
     if (stage == "2") {
-      var bob_public = await private_message.methods.bob_public().call()
-      message['bob_public'] = bob_public
+      var bob_public = await private_message.methods.bob_public().call();
+      message["bob_public"] = bob_public;
     }
-    message['address'] = private_message_addr
-    message['id'] = index;
+    message["address"] = private_message_addr;
+    message["id"] = index;
     messages.push(message);
   }
-  console.log("Messages", messages)
+  console.log("Messages", messages);
   return messages;
-};
-
+}
 
 export async function private_message_bob_stage_2(private_message_addr) {
-
-  var privateKey = w3.utils.randomHex(32)
+  var privateKey = w3.utils.randomHex(32);
   const ec = new EC("secp256k1");
   const ephemPrivKey = ec.keyFromPrivate(privateKey);
   const ephemPubKey = ephemPrivKey.getPublic();
   const ephemPubKeyEncoded = Buffer.from(ephemPubKey.encode());
-  const pub_key_readable = Buffer.from(ephemPubKey.encode()).toString('hex')
+  const pub_key_readable = Buffer.from(ephemPubKey.encode()).toString("hex");
 
-
-  window.db[pub_key_readable] = privateKey
+  window.db[pub_key_readable] = privateKey;
   window.localStorage.setItem(pub_key_readable, privateKey);
 
-
-  var accounts = await w3.eth.getAccounts()
-  await window.ethereum.enable()
-  var send = await contract.methods.pm_bob_reply(private_message_addr, pub_key_readable).send({ gasPrice: 0, from: accounts[0] })
-
+  var accounts = await w3.eth.getAccounts();
+  await window.ethereum.enable();
+  var send = await contract.methods
+    .pm_bob_reply(private_message_addr, pub_key_readable)
+    .send({ gasPrice: 0, from: accounts[0] });
 }
 function hexToRgb(hex) {
-  if (!hex || hex === undefined || hex === '') {
+  if (!hex || hex === undefined || hex === "") {
     return undefined;
   }
 
-  const result =
-    /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : undefined;
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : undefined;
 }
 function rgbToYIQ({ r, g, b }) {
-  return ((r * 299) + (g * 587) + (b * 114)) / 1000;
+  return (r * 299 + g * 587 + b * 114) / 1000;
 }
 
-export function contrast(colorHex: string | undefined,
-  threshold: number = 128): string {
+export function contrast(
+  colorHex: string | undefined,
+  threshold: number = 128
+): string {
   if (colorHex === undefined) {
-    return '#000';
+    return "#000";
   }
 
   const rgb: RGB | undefined = hexToRgb(colorHex);
 
   if (rgb === undefined) {
-    return '#000';
+    return "#000";
   }
 
-  return rgbToYIQ(rgb) >= threshold ? '#000' : '#fff';
+  return rgbToYIQ(rgb) >= threshold ? "#000" : "#fff";
 }
 
-var ColorHash = require('color-hash');
+var ColorHash = require("color-hash");
 export var colorHash = new ColorHash();
 
 export async function web3init() {
   if (!window.ethereum) {
     return false;
   }
-  await window.ethereum.enable();//'https://rpc.goerli.mudit.blog/');
-  var account = await w3.eth.getAccounts()
-  contract.options.from = account[0]
+  await window.ethereum.enable(); //'https://rpc.goerli.mudit.blog/');
+  var account = await w3.eth.getAccounts();
+  contract.options.from = account[0];
   users_address = account[0];
   return true;
 }
-
