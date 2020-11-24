6338faf36025670dec4901dab08c4d2228c58796
Mike Seiler <snissn@gmail.com>
2020-11-24 01:55:11 -0500

    update version and network label

--- src/Txtrs.js
+++ src/Txtrs.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import Panel from 'react-bootstrap/lib/Panel'
-import Button from 'react-bootstrap/lib/Button'
-import CustomerDetails from './CustomerDetails'
-import SendPublicMessage from "./SendPublicMessage"
-import PublicMessages from "./PublicMessages"
-import SendPrivateMessage from './SendPrivateMessage'
-import Messages from './Messages'
-import ReceivedMessages from './ReceivedMessages'
-import Conversations from './Conversations'
-import NewSendMessage from "./NewSendMessage"
+import React, { Component } from "react";
+import Panel from "react-bootstrap/lib/Panel";
+import Button from "react-bootstrap/lib/Button";
+import CustomerDetails from "./CustomerDetails";
+import SendPublicMessage from "./SendPublicMessage";
+import PublicMessages from "./PublicMessages";
+import SendPrivateMessage from "./SendPrivateMessage";
+import Messages from "./Messages";
+import ReceivedMessages from "./ReceivedMessages";
+import Conversations from "./Conversations";
+import NewSendMessage from "./NewSendMessage";
 
-
-import { getContract, contract, web3init } from "./Web3Helper"
+import { getContract, contract, web3init } from "./Web3Helper";
 
 export default class Arts extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      networkname: 'private'
-    }
+      networkname: "private",
+    };
   }
 
   //function which is called the first time the component loads
@@ -27,100 +25,126 @@ export default class Arts extends Component {
     var did_init = await web3init();
     if (did_init) {
       var networkname = await window.w3.eth.net.getNetworkType();
-      this.setState({ 'networkname': networkname })
+      this.setState({ networkname: networkname });
 
-      const response = await this.getPublicMessages()
-      console.log('response is ', response);
-      this.setState({ publicMessages: { "data": response } })
+      const response = await this.getPublicMessages();
+      console.log("response is ", response);
+      this.setState({ publicMessages: { data: response } });
     } else {
-      this.setState({ networkname: 'no-web3' })
+      this.setState({ networkname: "no-web3" });
     }
-
   }
 
   //Function to get the Art Data from json
   async getPublicMessages() {
-    if (this.state.networkname != 'private') {
+    if (this.state.networkname != "private") {
       return {};
     }
-    var messages_count = await contract.methods.get_public_message_count().call();
+    var messages_count = await contract.methods
+      .get_public_message_count()
+      .call();
 
     console.log("messages count", messages_count);
-    var messages = []
+    var messages = [];
     var counter = 0;
     for (var index = messages_count - 1; index >= 0; index--) {
       counter += 1;
-      if (counter == 5) { break; }
-      console.log("index is", index)
-      var message = await contract.methods.get_public_message_message(index).call()
-      var sender = await contract.methods.get_public_message_sender(index).call()
-      messages.push({ message: message, sender: sender, id: index })
+      if (counter == 5) {
+        break;
+      }
+      console.log("index is", index);
+      var message = await contract.methods
+        .get_public_message_message(index)
+        .call();
+      var sender = await contract.methods
+        .get_public_message_sender(index)
+        .call();
+      messages.push({ message: message, sender: sender, id: index });
     }
     return messages;
-
-  };
-
-  render() {
-    if (this.state.networkname == 'no-web3') {
-      return (<div><h1>
-
-        <a href="https://metamask.io" target="_blank">
-          Install MetaMask to use the site
-      </a>
-        <a className="cover" href="https://metamask.io" target="_blank">
-          <img src="/assets/images/download-extension.png" />
-        </a>
-
-      </h1>
-      </div>)
-    }
-    if (this.state.networkname != 'private') {
-      return (<div>
-        <h1>Metamask is on the wrong network </h1>
-        <h1>Switch to Dedicated Txt.rs sidechain!</h1>
-        <h2>Add new RPC URL:</h2>
-        <h4 className="App-subtitle">  <br />http://chain.txt.rs/ </h4>
-        <h2>Add new Chain ID:</h2>
-        <h4 className="App-subtitle">  <br />1337</h4>
-        <img className="metamask" src="/assets/images/metamask_change.png" />
-        <img className="metamask" src="/assets/images/metamask_example.png" />
-        <img className="metamask" src="/assets/images/metamask_settings.png" />
-      </div>)
-    }
-    if (!this.state.publicMessages) {
-      return (<p>Loading data</p>)
-    }
-    return (<div className="addmargin">
-      <div className="col-md-3">
-        <div>
-          <SendPublicMessage />
-          <PublicMessages />
-        </div>
-      </div>
-      <div className="col-md-3">
-        <div>
-          <h2>Encrypted Conversations</h2>
-          <Conversations />
-        </div>
-      </div>
-      <div className="col-md-6">
-        <div className="row">
-          <Messages />
-        </div>
-        <div className="col-md-12">
-          <div className="col-md-6">
-            <p>Outgoing Secure Messages</p>
-            <NewSendMessage />
-          </div>
-          <div className="col-md-6">
-            <p>Incoming Secure Messages</p>
-            <ReceivedMessages />
-          </div>
-        </div>
-      </div>
-    </div>)
   }
 
+  render() {
+    if (this.state.networkname == "no-web3") {
+      return (
+        <div>
+          <h1>
+            <a href="https://metamask.io" target="_blank">
+              Install MetaMask to use the site
+            </a>
+            <a className="cover" href="https://metamask.io" target="_blank">
+              <img src="/assets/images/download-extension.png" />
+            </a>
+          </h1>
+        </div>
+      );
+    }
+    if (this.state.networkname != "private") {
+      return (
+        <div>
+          <h1>Metamask is on the wrong network </h1>
+          <h1>Switch to Dedicated Txt.rs sidechain!</h1>
+          <h2>Network Name:</h2>
+          <h4 className="App-subtitle">
+            {" "}
+            <br />
+            Txt{" "}
+          </h4>
+
+          <h2>New RPC URL:</h2>
+          <h4 className="App-subtitle">
+            {" "}
+            <br />
+            http://chain.txt.rs/{" "}
+          </h4>
+          <h2>Chain ID:</h2>
+          <h4 className="App-subtitle">
+            {" "}
+            <br />
+            1337
+          </h4>
+          <img className="metamask" src="/assets/images/metamask_change.png" />
+          <img className="metamask" src="/assets/images/metamask_example.png" />
+          <img
+            className="metamask"
+            src="/assets/images/metamask_settings.png"
+          />
+        </div>
+      );
+    }
+    if (!this.state.publicMessages) {
+      return <p>Loading data</p>;
+    }
+    return (
+      <div className="addmargin">
+        <div className="col-md-3">
+          <div>
+            <SendPublicMessage />
+            <PublicMessages />
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div>
+            <h2>Encrypted Conversations</h2>
+            <Conversations />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="row">
+            <Messages />
+          </div>
+          <div className="col-md-12">
+            <div className="col-md-6">
+              <p>Outgoing Secure Messages</p>
+              <NewSendMessage />
+            </div>
+            <div className="col-md-6">
+              <p>Incoming Secure Messages</p>
+              <ReceivedMessages />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
