226c95b2a9c3478c7602e7a6b50f0a288fa2fa19
snissn <snissn@gmail.com>
2020-12-01 22:59:10 -0500

    Update SendPublicMessage.js

--- src/components/SendPublicMessage.js
+++ src/components/SendPublicMessage.js
@@ -24,7 +24,7 @@ export default function SendPublicMessage() {
         .then((gasEstimate) => {
           contract.methods
             .send_public_message(message)
-            .send({ gas: gasEstimate }); //, {from:account}).send({from:account, value:0})
+            .send({ gas: gasEstimate });
         });
     }
   };
