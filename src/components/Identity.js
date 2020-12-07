4d1ab369bf32b15083d9150afb4e9dedcd9b43e4
Mike Seiler <snissn@gmail.com>
2020-12-06 20:55:59 -1000

    navbar

---
+++ src/components/Identity.js
@@ -0,0 +1,22 @@
+import React, { Component, useState, useEffect } from "react";
+import { w3 } from "../helpers/Web3Helper";
+
+export default function Identity() {
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      var x = await w3.eth.getAccounts();
+      console.log("X", x);
+      setAccount(x);
+      // ...
+    }
+    fetchData();
+  }, [account]); // Or [] if effect doesn't need props or state
+
+  if (!account) {
+    return <div />;
+  }
+  return <div>{account[0]}</div>;
+}
