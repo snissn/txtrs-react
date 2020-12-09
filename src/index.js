14483f19a1df2fef1aad167dc0dbdef251059d9a
Gabe Gralla <gabe.gralla@gmail.com>
2020-12-09 18:46:31 +0000

    remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
@@ -11,4 +10,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-registerServiceWorker();
